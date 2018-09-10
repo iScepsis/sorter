19202da8417b2e1e9ac1712afc6081a40138d815
scepsis <f.senin@nextcontact.su>
2018-09-10 17:57:36 +0300

    наработки

--- sorter.sh
+++ sorter.sh
@@ -3,14 +3,40 @@
 
 source bin/file_types
 source bin/functions/check_directory
+source bin/functions/create_output_directory
 
-export CURRENT_FILE_TYPE="txt"
+export CURRENT_FILE_TYPE=""
+export SORTING_PATH=$HOME/Documents/LM
+export OUTPUT_DIR=$SORTING_PATH/Sorted
 
 #check_directory
 echo ${target_directories[0]}
 echo ${mime_types[1]}
 
-for file in ~/Documents/*
+while getopts u:d:p:f: option
 do
-    echo "$file"
+    case "${option}"
+    in
+        p) SORTING_PATH=$OPTARG;;
+        d) OUTPUT_DIR=$OPTARG;;
+    esac
 done
+
+echo "Current sorting path: $SORTING_PATH"
+echo "::::::::::::::::START SORTING:::::::::::::::"
+
+create_output_directory
+
+for file in $SORTING_PATH/*
+do
+    #if [[ -d $file ]] ; then
+    #    echo "$file is directory"
+    #fi
+    if [[ -f $file ]] ; then
+        CURRENT_FILE_TYPE=$(file -b --mime-type "$file")
+        check_directory
+        echo $CURRENT_FILE_TYPE
+    fi
+done
+
+echo "::::::::::::::SORTING IS FINISH::::::::::::"
